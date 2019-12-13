a417a65c061e692860eddbd944ce403a01522389
Borgee <rjdzgn@gmail.com>
2019-12-13 22:22:56 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //Filter Search
-function myFunction() {
+function searchQuestion() {
   // Declare variables
   var input, filter, ul, li, a, i, button, txtValue;
   input = document.getElementById('myInput');
@@ -9,7 +9,8 @@ function myFunction() {
 
   // Loop through all list items, and hide those who don't match the search query
   for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("button")[0];
+    a = li[i].getElementsByTagName("button")[0],
+    a = li[i].getElementsByTagName("p")[0];
     txtValue = a.textContent || a.innerText;
     if (txtValue.toUpperCase().indexOf(filter) > -1) {
       li[i].style.display = "";

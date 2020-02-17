407f6f028611286d9b7c58dd2a70257d82e312d3
Borgee <rjdzgn@gmail.com>
2020-02-17 22:01:57 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,13 @@
 //Filter Search
-function searchQuestion() {
-  // Declare variables
-  var input, filter, ul, li, a, i, button, txtValue;
-  input = document.getElementById('myInput');
-  filter = input.value.toUpperCase();
-  ul = document.getElementById("questions");
-  li = ul.getElementsByTagName('li');
+$(document).ready(function(){
+  $("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#questions li").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
 
-  // Loop through all list items, and hide those who don't match the search query
-  for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("button")[0],
-    a = li[i].getElementsByTagName("p")[0];
-    txtValue = a.textContent || a.innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = "";
-    } else {
-      li[i].style.display = "none";
-    }
-  }
-}
 
 //Toggle Dropdown
 var acc = document.getElementsByClassName("accordion");
